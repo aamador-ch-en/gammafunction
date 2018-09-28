695cf2a5d980f1864b436bb68aa7117d2a8ac9d1
Abraham Amador <aamador994@gmail.com>
2018-09-28 15:18:23 -0500

    Create pch.h

---
+++ pch.h
@@ -0,0 +1,29 @@
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+#ifndef PCH_H
+#define PCH_H
+
+class forgam
+{
+private: 
+	//variables for functions
+	double zfact;
+	double z;
+	double x;
+	double zed;
+	double k;
+public: //functions to define the value	
+	int natre(double z);
+	double fx(double x, double z);
+	double gamma(double zed, double k);
+	double factorial(double zed);
+};
+
+
+#endif //PCH_H
